64427b2d51e6047424f1f907e307f8f635082aed
Ben Bovee <principal@patterndigm.com>
2016-11-30 12:37:07 -0500

    Create main.rb

---
+++ main.rb
@@ -0,0 +1,17 @@
+# Logic Operators Program in Ruby
+
+puts "p q and or xor nand nor"; #Header row
+
+# ".to_S" 'hard types' a character string to a String.
+
+def band (a, b)
+  if a 
+  return c
+end #band
+
+for p in 0..1
+  for q in 0..1
+    # puts (p.to_s + ' ' + q.to_s + ' ' + (p and q) + ' ' + (p or q) + ' ')
+    puts p.to_s + ' ' + q.to_s
+  end               #next p
+end               #next q
